0e805f7e148711d848d5c4afb29f47454f29b279
Andre Kiona <124687319+andreKiona@users.noreply.github.com>
2025-05-05 19:07:57 +0200

    signUP

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+
   return <div>SignUpPage</div>;
 };
 
